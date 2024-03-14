5b43d9f6d1e169861e8827cef610e058cd13fc22
itnanunes <nunesitna@gmail.com>
2024-03-14 17:36:24 -0300

    Cenários negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,47 @@
 /// <reference types="cypress"/>
 
 describe('Funcionalidade: Login', ()=> {
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+     });
 
+     afterEach(() => {
+        cy.screenshot()
+     });
 it ('Deve fazer login com sucesso', () => {
- cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
  cy.get('#username').type('itna.teste@teste.com')
  cy.get('#password').type('teste@123')
  cy.get('#rememberme').check()
  cy.get('.woocommerce-form > .button').click()
  cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, itna.teste (não é itna.teste? Sair)')
 })
+
+it('Deve exibir uma mensagem de erro ao inserir um usuário inválido', () => {
+    cy.get('#username').type('itna.teste@test.com')
+    cy.get('#password').type('teste@123')
+    cy.get('#rememberme').check()
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+
+});
+
+it('Deve exibir uma mensagem de erro ao inserir senha incorreta', () => {
+    cy.get('#username').type('itna.teste@teste.com')
+    cy.get('#password').type('12345')
+    cy.get('#rememberme').check()
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail itna.teste@teste.com está incorreta. Perdeu a senha?')
+});
+
+it('Deve exibir uma mensagem de erro ao Deixar campos em branco', () => {
+    cy.get('#username').type(' ')
+    cy.get('#password').type(' ')
+    cy.get('#rememberme').check()
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: Nome de usuário é obrigatório.')
+});
+
+
 
 })
 
